1e5c2981fab00f7bd283c1999452ebd94d464d64
hemant <jangrahemant6@gmail.com>
2022-04-06 23:52:57 +0530

    form validation

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -3,15 +3,42 @@ import { TextField, InputAdornment, IconButton, Button, Stack, Grid, Container, 
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import EmailIcon from '@mui/icons-material/Email';
 import { green, red, grey } from '@mui/material/colors';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
+
+
 
 export function LoginForm() {
+  const navigate = useNavigate()
+  const loginData = {
+    email: '',
+    password: '',
+  };
 
-  
+  const loginDataSchema = Yup.object().shape({
+    email: Yup.string().email('Must be a valid email').max(45).required('Email is required'),
+    password: Yup.string().max(45).min(6, 'password must be at least 6 characters').required('Password is required')
+  });
+  const formik = useFormik({
+    initialValues: loginData,
+    validationSchema: loginDataSchema,
+    onSubmit: () => {
+      alert(JSON.stringify(formik.values, null, 2));
+      navigate('/home');
+      
+    }
+  });
+
+
   return (
     <Paper sx={{ p: 4 }} elevation={4}>
       <Stack maxWidth={400} spacing={2} >
         <Grid item xs={12} md={4} >
           <TextField
+          {...formik.getFieldProps('email')}
+          error={Boolean(formik.touched.email && formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
           sx={{borderBlockColor:'red'}}
             label="email"
             fullWidth
@@ -26,9 +53,12 @@ export function LoginForm() {
         </Grid>
         <TextField
           fullWidth
+          {...formik.getFieldProps('password')}
           label="password"
           id="outlined-start-adornment"
           variant="outlined"
+          error={Boolean(formik.touched.password && formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
           InputProps={{
             endAdornment: <InputAdornment position="end">
               <IconButton style={{ color: grey[500] }} >
@@ -39,6 +69,8 @@ export function LoginForm() {
         />
         <Grid container justifyContent="center">
           <Button
+          type="submit"
+          onClick={formik.handleSubmit}
             sx={{ borderRadius: "16px",textTransform: 'none' }}
             variant="contained"><Typography>Log In</Typography></Button>
         </Grid>
