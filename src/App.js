e4bc4b3c526a688b7ce51d6a92f08813fdaa9a87
hemant <jangrahemant6@gmail.com>
2022-04-06 19:32:54 +0530

    add styled -components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
+const Button = styled.button`
+background: transparent;
+border-radius: 3px;
+border: 2px solid palevioletred;
+color: palevioletred;
+margin: 0 1em;
+padding: 0.25em 1em;
+`
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wint-assignment">
+      <h1>
+        this works
+      </h1>
+      <Button>First Styled Button</Button>
     </div>
   );
 }
