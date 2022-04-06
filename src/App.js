7d76b4e296695c46205f7ffcee97404c03b23883
hemant <jangrahemant6@gmail.com>
2022-04-06 21:02:22 +0530

    creating ui elements from scratch.

--- src/App.js
+++ src/App.js
@@ -7,15 +7,73 @@ border: 2px solid palevioletred;
 color: palevioletred;
 margin: 0 1em;
 padding: 0.25em 1em;
+width: 50%;
 `
+
+
+const LoginForm = styled.div`
+display : flex;
+flex-direction: column;
+direction : vertical;
+row-gap : 10px;
+max-width : 200px;
+align-items : center;
+padding: 20px;
+`;
+
+const EyeIcon = styled.i`
+"margin-left: -30px;
+cursor: pointer;
+`;
+
+const TextField = styled.div`
+border: 1px solid;
+font-weight: 400;
+font-size: 1rem;
+line-height: 1.4375em;
+letter-spacing: 0.00938em;
+color: rgba(0, 0, 0, 0.87);
+box-sizing: border-box;
+cursor: text;
+display: inline-flex;
+-webkit-box-align: center;
+align-items: center;
+position: relative;
+border-radius: 4px;
+padding-right: 14px;
+`;
+const Input = styled.input`
+border: none;
+&:focus {
+  outline: none;
+  border: none;
+}
+&:hover {
+  outline: none;
+  border: none;
+}
+
+`;
+const ButtonIcon = styled.button`
+border: none;
+background-color:transparent;
+`;
+
 function App() {
   return (
-    <div className="wint-assignment">
-      <h1>
-        this works
-      </h1>
-      <Button>First Styled Button</Button>
-    </div>
+    <LoginForm>
+      <Input error="error" placeholder="username" />
+      <TextField class="input-container">
+        <Input placeholder="password" />
+      </TextField>
+      <TextField style={{display: "flex"}}>
+        <Input placeholder='password'/>
+        <ButtonIcon>
+          <i class="far fa-eye" id="togglePassword" ></i>
+        </ButtonIcon>
+      </TextField>
+      <Button>Log In</Button>
+    </LoginForm>
   );
 }
 
