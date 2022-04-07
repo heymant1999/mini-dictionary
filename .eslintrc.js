546aca90c0864867f94268df904ee80e7d51aaf5
hemant <jangrahemant6@gmail.com>
2022-04-07 22:59:23 +0530

    es lint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,37 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: ['react', 'import'],
+  plugins: ["react", "import"],
   rules: {
-    'import/prefer-default-export': 0,
-    'comma-dangle': 0,
-    'no-unused-vars': 0,
-    'react/react-in-jsx-scope': 0,
-    'import/extensions': 0,
-    'react/jsx-filename-extension': 0,
-    'react/jsx-props-no-spreading': 0,
-    'object-curly-newline': 0,
-    'import/no-cycle': 0,
-    'no-use-before-define': 0,
-    'react/no-array-index-key': 0,
-    'no-param-reassign': 0
+    "import/prefer-default-export": 0,
+    "comma-dangle": 0,
+    "no-unused-vars": 0,
+    "react/react-in-jsx-scope": 0,
+    "import/extensions": 0,
+    "react/jsx-filename-extension": 0,
+    "react/jsx-props-no-spreading": 0,
+    "object-curly-newline": 0,
+    "import/no-cycle": 0,
+    "no-use-before-define": 0,
+    "react/no-array-index-key": 0,
+    "no-param-reassign": 0,
   },
   settings: {
-    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx', '.js', '.jsx']
-    },
-    'import/resolver': {
+    "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+    "import/resolver": {
       node: {
-        extensions: ['.js', '.ts', '.tsx', '.jsx']
-      }
-    }
-  }
+        extensions: [".js", ".ts", ".tsx", ".jsx"],
+      },
+    },
+  },
 };
