050a2cf33be4e9dea611accc6863783c5911f970
heymant1999 <jangrahemant6@gmail.com>
2024-12-18 10:37:13 +0530

    migrate to createRoot

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
+import { createRoot } from "react-dom/client";
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const app = document.getElementById("root");
+const root = createRoot(app);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
